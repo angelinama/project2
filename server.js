6c145f95381e440afc546351c69e11a5497c864a
binm <binm@amazon.com>
2021-02-24 09:56:44 -0800

    set up start script and server.js to only render index.html

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+// Sets up the Express App
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Static directory
+app.use(express.static('public'));
+
+
+// // Syncing our sequelize models and then starting our Express app
+// db.sequelize.sync({ force: true }).then(() => {
+//   app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
+// });
+
+app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
