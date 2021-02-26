53dfaaff93b4f543cfa6ae944e7e0bcbb1072919
Carolina R <carolinarizk@gmail.com>
2021-02-26 06:06:24 -0500

    set handlebars and required its files

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const express = require("express");
 
+//import routes --Carolina
+const initRoute = require("./routes/init.js");
+const loginRoute = require("./routes/login.js");
+const signupRoute = require("./routes/signup.js");
+
 // Sets up the Express App
 const app = express();
 const PORT = process.env.PORT || 8080;
@@ -11,11 +16,14 @@ app.use(express.json());
 // Static directory
 app.use(express.static("public"));
 
-// Set Handlebars.
+// Set Handlebars. --Carolina
 const exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
+initRoute(app);
+loginRoute(app);
+signupRoute(app);
 
 // // Syncing our sequelize models and then starting our Express app
 // db.sequelize.sync({ force: true }).then(() => {
