f3fdb348b52b84d35cf12a5bc63d2849aecbff7a
Carolina R <carolinarizk@gmail.com>
2021-02-27 11:18:32 -0500

    condensed init, signup and login routes into login

--- routes/init.js
+++
@@ -1,6 +0,0 @@
-//render the page in browser
-module.exports = (app) => {
-  app.get("/", (req, res) => {
-    res.render("../views/init.handlebars");
-  });
-};
--- routes/signup.js
+++
@@ -1,6 +0,0 @@
-//render the page in browser
-module.exports = (app) => {
-  app.get("/signup", (req, res) => {
-    res.render("../views/signup.handlebars");
-  });
-};
