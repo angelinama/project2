b629f1abadd7cb7e7478f08fbe3dd6c28e326772
banuakman <banu@asterya.com>
2021-02-27 13:05:26 -0500

    combine all html routes in one file

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,5 +1,17 @@
 module.exports = (app) => {
   //render the welcome page in browser
+  app.get("/", (req, res) => {
+    res.render("../views/init.handlebars");
+  });
+  //render the login page in browser
+  app.get("/login", (req, res) => {
+    res.render("../views/login.handlebars");
+  });
+  //render the signup page in browser
+  app.get("/signup", (req, res) => {
+    res.render("../views/signup.handlebars");
+  });
+  //render the welcome page in browser
   app.get("/welcome", (req, res) => {
     res.render("../views/welcome.handlebars");
   });
--- routes/login.js
+++
@@ -1,6 +0,0 @@
-//render the page in browser
-module.exports = (app) => {
-  app.get("/login", (req, res) => {
-    res.render("../views/login.handlebars");
-  });
-};
