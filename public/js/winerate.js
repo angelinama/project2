5fc5ea5baa8e5a8374395a8b536fd308ab1bc49f
banuakman <banu@asterya.com>
2021-02-28 01:13:25 -0500

    references to winerate form and input

--- public/js/winerate.js
+++ public/js/winerate.js
@@ -4,6 +4,18 @@ $(document).ready(() => {
   const wineratingForm = $("form.winerate");
   const overAllRating = $("input.allrating");
   const addFavorite = $("input#addfavorite");
-  const tasteFruit = $("input#fruit");
-  const tasteHerbal = $("input#herbal");
+  const aromaFruit = $("input#fruit");
+  const aromaHerbal = $("input#herbal");
+  const aromaEarth = $("input#earth");
+  const aromaFloral = $("input#floral");
+  const aromaSpice = $("input#spice");
+  const aromaChemical = $("input#chemical");
+  const tasteSweetness = $("input.sweetness");
+  const tasteAcidity = $("input.acidity");
+  const tasteTannins = $("input.tannins");
+  const tasteBody = $("input.wbody");
+});
+wineratingForm.on("submit", (event) => {
+  event.preventDefault();
+
 });
