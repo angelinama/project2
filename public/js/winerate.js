3e52a2585e8ba3c139382cdd09b28b9c1271bd82
banuakman <banu@asterya.com>
2021-02-28 00:26:55 -0500

    start winerate frontend js

---
+++ public/js/winerate.js
@@ -0,0 +1,9 @@
+$(document).ready(() => {
+  console.log("start rating wine");
+  // Getting references to our form and input
+  const wineratingForm = $("form.winerate");
+  const overAllRating = $("input.allrating");
+  const addFavorite = $("input#addfavorite");
+  const tasteFruit = $("input#fruit");
+  const tasteHerbal = $("input#herbal");
+});
