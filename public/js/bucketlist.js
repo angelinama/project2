bfcdc09bcc6bfcc3300a33e2a6f19d6f752375fd
Carolina R <carolinarizk@gmail.com>
2021-03-01 11:51:33 -0500

    working on moving bucket to history

--- public/js/bucketlist.js
+++ public/js/bucketlist.js
@@ -29,14 +29,12 @@ $(document).ready(() => {
       button.addEventListener("click", (e) => {
         // Grabs the id of the element that goes by the name, "id"
         const id = e.target.getAttribute("data-id");
-
-        fetch(`/api/bucketlists/${id}`, {
-          method: "DELETE",
-        }).then((response) => {
+        const wineId = e.target.getAttribute("data-wine");
+        $.post("/api/reviews", { wine_id: wineId }).then((response) => {
           // Check that the response is all good
           // Reload the page so the user can see the new state
           if (response.ok) {
-            console.log(`deleted wine: ${id}`);
+            console.log(`moved wine: ${id}`);
             location.reload("/");
           } else {
             alert("something went wrong!");
