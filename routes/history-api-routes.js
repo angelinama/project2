3dd87793dc4a0335036e3b86f07a304d7ee6625c
banuakman <banu@asterya.com>
2021-03-01 13:46:10 -0500

    add api route to get history by user id

--- routes/history-api-routes.js
+++ routes/history-api-routes.js
@@ -1,5 +1,23 @@
 const db = require("../models");
-const Sequelize = require("sequelize");
+
 module.exports = (app) => {
-    
+  //get user's history
+  app.get("/api/history/:id", (req, res) => {
+    if (req.user) {
+      db.History.findAll({
+        where: {
+          user_id: req.params.id,
+        },
+      })
+        .then((history) => {
+          res.json(history);
+        })
+        .catch((err) => {
+          console.log(err);
+          res.status(500).send("Something went wrong");
+        });
+    } else {
+      res.status(401).json({ error: "Please log in" });
+    }
+  });
 };
