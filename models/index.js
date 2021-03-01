4cdfb500613cb62160b937ad0a4c940a8630d252
Angelina Ma <angelina890308@gmail.com>
2021-03-01 00:21:09 -0800

    fix Heroku deploymentcrash

--- models/index.js
+++ models/index.js
@@ -10,12 +10,7 @@ const db = {};
 
 let sequelize;
 if (config.use_env_variable) {
-  sequelize = new Sequelize(
-    config.database,
-    config.username,
-    process.env[config.use_env_variable],
-    config
-  );
+  sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
   sequelize = new Sequelize(
     config.database,
